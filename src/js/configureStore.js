8028cea8caa05138606831d70963d318b6de13f6
Peter Swartwout <Peter@peters-MBP.fios-router.home>
2016-12-12 20:43:13 -0500

    Adds a "real" back-end (it's just a json file) to impersonate an API.

--- src/js/configureStore.js
+++ src/js/configureStore.js
@@ -1,29 +1,18 @@
-import { createStore } from "redux";
-import throttle from "lodash/throttle";
+import thunkMiddleware from "redux-thunk";
+import createLogger from "redux-logger";
+import { createStore, applyMiddleware, compose } from "redux";
+import { requestAttendanceStats } from "./actions";
 import drawingApp from "./reducers";
-import { loadState, saveState, clearState } from "./utilities/localStorage";
-import { fetchAttendanceData } from "./api";
 
 const configureStore = () => {
-    let persistedState = loadState();
-    let apiAttendanceData = fetchAttendanceData();
 
-    if (!persistedState || !persistedState.metadata || persistedState.metadata.version != apiAttendanceData.metadata.version) {
-        // The json has changed. (or never existed) Replace local storage with it.
-        persistedState = apiAttendanceData;
-        // Just in case the shape of the json change also, clear it.
-        clearState();
-        // Now refresh local storage from json
-        saveState(apiAttendanceData);
-    }
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(drawingApp, composeEnhancers(applyMiddleware(thunkMiddleware, createLogger())));
 
-    const store = createStore(drawingApp, persistedState, window.devToolsExtension ? window.devToolsExtension() : undefined);
-
-    store.subscribe(throttle(() => {
-        saveState(store.getState());
-    }, 1000));
-
+    // Read json "database" for the first time
+    store.dispatch(requestAttendanceStats());
     return store;
 };
 
 export default configureStore;
+
--- src/js/reducers/club.js
+++ src/js/reducers/club.js
@@ -1,6 +1,10 @@
 const club = (state = {}, action) => {
     let newState = state;
     switch (action.type) {
+        case 'RECEIVE_ATTENDANCE_STATS':
+            // New JSON has arrived from the API
+            newState = Object.assign({}, state, action.attendance.club);
+            break;
     }
     return newState;
 }
