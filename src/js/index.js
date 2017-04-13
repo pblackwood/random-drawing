d69ceee3fac75d19e63bac2f15ceca721fffdfe4
Peter Swartwout <Peter@peters-MBP.fios-router.home>
2017-04-13 08:20:39 -0400

    Updates webpack to 2.2. Moves html, css, and assets into src.

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "react-dom";
 import Root from "./components/Root";
 import configureStore from "./configureStore";
+import css from "../less/app.less";
 
 render(<Root store={configureStore()}/>, document.getElementById('attendanceStats'))
 
